871608bb7ff8ffc8da3f082748d362dc4f60a7a3
Loriano Storchi <loriano@storchi.org>
2024-11-08 18:20:23 +0100

    backup

---
+++ extract.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+val1=$(grep "using PLS SS MAPE" modelsresults.csv | awk -F, '{print $3}')
+val2=$(grep "using PLS SS split MAPE" modelsresults.csv | awk -F, '{print $3}')
+val3=$(grep "using LR SS MAPE" modelsresults.csv | awk -F, '{print $3}')
+val4=$(grep "using LR SS split MAPE" modelsresults.csv | awk -F, '{print $3}')
+val5=$(grep "using Custom LR SS MAPE" modelsresults.csv | awk -F, '{print $3}')
+val6=$(grep "using Custom LR SS split MAPE" modelsresults.csv | awk -F, '{print $3}')
+val7=$(grep "using PLS Full MAPE" modelsresults.csv | awk -F, '{print $3}')
+val8=$(grep "using PLS Full split MAPE" modelsresults.csv | awk -F, '{print $3}')
+val9=$(grep "using LR Full MAPE" modelsresults.csv | awk -F, '{print $3}')
+val10=$(grep "using LR Full split MAPE" modelsresults.csv | awk -F, '{print $3}')
+val11=$(grep "using Custom LR Full MAPE" modelsresults.csv | awk -F, '{print $3}')
+val12=$(grep "using Custom LR Full split MAPE" modelsresults.csv | awk -F, '{print $3}')
+val13=$(grep "using PLSRF MAPE" modelsresults.csv | awk -F, '{print $3}')
+val14=$(grep "using PLSRF split MAPE" modelsresults.csv | awk -F, '{print $3}')
+val15=$(grep "using LRRF MAPE" modelsresults.csv | awk -F, '{print $3}')
+val16=$(grep "using LRRF split MAPE" modelsresults.csv | awk -F, '{print $3}')
+val17=$(grep "using Custom LRRF MAPE" modelsresults.csv | awk -F, '{print $3}')
+val18=$(grep "using Custom LRRF split MAPE" modelsresults.csv | awk -F, '{print $3}')
+
+echo $val1 , $val2 , $val3 , $val4 , $val5 , $val6 , $val7 , $val8 , $val9 , $val10 , $val11 , $val12 , $val13 , $val14 , $val15 , $val16 , $val17 , $val18
