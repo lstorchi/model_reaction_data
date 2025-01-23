a8665422f8ef3370bc104cf916309a06a0a30428
Loriano Storchi <loriano@storchi.org>
2025-01-23 09:56:02 +0100

    scripts

---
+++ all.sh
@@ -0,0 +1,8 @@
+echo "NO FLPs"
+./runall.sh generaldataset_LR_work1_noflps.py
+echo "FLPs"
+./runall.sh generaldataset_LR_work1_flps.py
+echo "NO FLPs Beta One"
+./runall.sh generaldataset_LR_work1_noflps_betaone.py
+echo "FLPs Beta One"
+./runall.sh generaldataset_LR_work1_flps_betaone.py
